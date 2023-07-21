1d5952d41e5cce0f5bb27b7b458fec74e7a64621
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-07-21 17:18:20 +0700

    benerin error penamaan class

--- client/src/components/layouts/ProductPotong.jsx
+++ client/src/components/layouts/ProductPotong.jsx
@@ -4,7 +4,7 @@ import Card from "../fragments/Card";
 const ProductPotong = () => {
   return(
     <div className="product-potong">
-      <Title classname='potong' title='Produk' subTitle='Jenis Potongan' />
+      <Title classname='title-section potong' title='Produk' subTitle='Jenis Potongan' />
       <section className="cards-product">
         <Card tipe={'Slice Dadu'} berat={500} image={"./images/product-3.jpg"} />
         <Card tipe={'Slice Tipis'} berat={500} image={"./images/product-1.jpg"} />
