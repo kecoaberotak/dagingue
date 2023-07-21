8d2e09374ef62995cd80dc08d143b98b88c9cc2f
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-07-21 22:31:47 +0700

    bikin route buat landing page dan error page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,16 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ImageSelectedContexProvider from './contexts/ImageSelected'
 
+// Pages
+import LandingPage from './pages/LandingPage'
+import ErrorPage from './pages/errorPage'
+
 // CSS
 import './assets/css/main.css'
 
-// Pages
-import LandingPage from './pages/LandingPage'
+// Router
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LandingPage />,
+    errorElement: <ErrorPage />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ImageSelectedContexProvider>
-      <LandingPage></LandingPage>
+      <RouterProvider router={router} />
     </ImageSelectedContexProvider>
   </React.StrictMode>,
 )
