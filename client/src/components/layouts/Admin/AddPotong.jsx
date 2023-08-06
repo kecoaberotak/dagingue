2eb3884e604c5c2267e428d2e897dd6776a077f7
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-08-06 20:45:30 +0700

    component addPotong kelar

--- client/src/components/layouts/Admin/AddPotong.jsx
+++ client/src/components/layouts/Admin/AddPotong.jsx
@@ -1,7 +1,87 @@
+import { useContext, useState } from 'react';
+import Button from '../../elements/button';
+import { DisplayStatus } from '../../../contexts/DisplayStatus';
+import { addPotong } from '../../../services/potong-service';
+
 const AddPotong = () => {
+  const {setDisplayStatus} = useContext(DisplayStatus);
+  const [buttonStatus, setButtonStatus] = useState('');
+  const [srcPreview, setSrcPreview] = useState();
+
+  const [title, setTitle] = useState('');
+  const [file, setFile] = useState('');
+  const [desc, setDesc] = useState();
+
+  const addNewPotong = (e) => {
+    e.preventDefault();
+    const data = new FormData();
+    data.set('title', title);
+    data.set('file', file[file.length - 1]);
+    data.set('desc', desc);
+
+    if(buttonStatus === 'submit'){
+      const data = new FormData();
+      data.set('title', title);
+      data.set('file', file[file.length - 1]);
+      data.set('desc', desc);
+  
+      if(!data.get('title') || data.get('file') === 'undefined' || data.get('desc') === 'undefined'){
+        alert('Masukkan Data Jenis Potongan!');
+      }else {
+        addPotong(data, res => {
+          if(res.status === 400){
+            alert(res.data.message)
+          }else if(res.status === 200){
+            alert(res.data.message);
+            setDisplayStatus('show');
+          }
+        });
+      }
+    }else if(buttonStatus === 'cancel'){
+      setDisplayStatus('show');
+    }
+  };
+
   return (
     <>
     <h1>Tambah Jenis Potongan</h1>
+    <form className="form-content" onSubmit={addNewPotong}>
+        <label htmlFor="jenis-potong">Jenis Potongan</label>
+        <input 
+          type="title" 
+          placeholder="Jenis Potongan" 
+          value={title} 
+          name='jenis-potong'
+          onChange={e => 
+          setTitle(e.target.value)}
+        />
+        <label htmlFor="desc-potong">Penjelasan Bumbu</label>
+        <input 
+          type="title" 
+          placeholder="Penjelasan jenis potongan" 
+          value={desc} 
+          name='desc-potong'
+          onChange={e => 
+          setDesc(e.target.value)}
+        />
+        <label htmlFor="gambar-potong"><p>Upload gambar jenis potongan</p></label>
+        {srcPreview && <img src={`${srcPreview}`} alt="Gambar Jenis Potongan" className='gambar-potong'/>}
+        <input 
+          type="file" 
+          name='gambar-potong' 
+          onChange={e => {
+            const objectUrl = URL.createObjectURL(e.target.files[0])
+            setSrcPreview(objectUrl);
+            setFile(e.target.files);
+          }}
+        />
+        <div className="form-button">
+          <Button onClick={() => setButtonStatus('submit')}>Submit</Button>
+          <div className="button-cancel">
+            <Button onClick={() => setButtonStatus('cancel')}>Cancel</Button>
+          </div>
+        </div>
+      </form>
     </>
   )
 }
