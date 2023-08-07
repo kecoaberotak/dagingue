6a301fdc4119f983235aa42999cfcd5afdffce16
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-08-07 12:24:27 +0700

    bikin routes about

---
+++ server/routes/aboutRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const {getAbout, getDetailAbout ,setAbout, updateAbout, deleteAbout} = require('../controllers/aboutController');
+
+// Multer - buat upload file
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
+
+router.route('/').get(getAbout).post(upload.single('file'), setAbout);
+router.route('/:id').get(getDetailAbout).put(upload.single('file'), updateAbout).delete(deleteAbout);
+
+module.exports = router;
