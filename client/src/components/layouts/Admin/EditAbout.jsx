fe84897369eef96b7d32cd18e06deae5136c0ee9
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-08-08 16:20:02 +0700

    berhasil nampilin data di form

--- client/src/components/layouts/Admin/EditAbout.jsx
+++ client/src/components/layouts/Admin/EditAbout.jsx
@@ -1,15 +1,119 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
+import Button from '../../elements/button';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import { DisplayStatus } from '../../../contexts/DisplayStatus';
 import { getContent } from '../../../services/about-service';
 
 const EditAbout = () => {
   const [data, setData] = useState();
+  const [idContent, setIdContent] = useState();
+  const [srcPreview, setSrcPreview] = useState();
+  const [srcPreview2, setSrcPreview2] = useState();
+  const {setDisplayStatus} = useContext(DisplayStatus);
+
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState();
+  const [file, setFile] = useState([]);
 
   useEffect(() => {
     getContent(res => setData(res.data[0]));
   }, []);
 
+  useEffect(() => {
+    if(data){
+      setTitle(data.title);
+      setDesc(data.desc);
+      setIdContent(data._id);
+      setSrcPreview(`http://localhost:4000/${data.file1}`);
+      setSrcPreview2(`http://localhost:4000/${data.file2}`);
+    }
+  }, [data]);
+
+  const modules = {
+    toolbar: [
+      [{ 'header': [1, 2, false] }],
+      ['bold', 'italic', 'underline','strike', 'blockquote'],
+      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+      ['link', 'image'],
+      ['clean']
+    ],
+  };
+  
+  const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+  ];
+
+  const editContent = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <h1>Edit Content About</h1>
+    <>
+      <h1>Edit Content About</h1>
+      <form className="form-content" onSubmit={editContent}>
+        <label htmlFor="judul-content">Judul Content</label>
+        <input 
+          type="title" 
+          placeholder="Judul Content" 
+          value={title} 
+          name='judul-content'
+          onChange={e => 
+          setTitle(e.target.value)}
+        />
+        <div className="upload-gambar-about">
+          <section className="upload-gambar">
+            <label htmlFor="gambar1"><p>Gambar content 1</p></label>
+            {srcPreview && <img src={`${srcPreview}`} alt="Gambar Content" className='gambar-content'/>}
+            <input 
+              type="file" 
+              name='gambar1' 
+              onChange={e => {
+                const newData = [...file];
+                newData[0] = e.target.files[0];
+                setFile(newData);
+                const objectUrl = URL.createObjectURL(e.target.files[0])
+                setSrcPreview(objectUrl);
+              }}
+            />
+          </section>
+          <section className="upload-gambar">
+            <label htmlFor="gambar2"><p>Gambar content 2</p></label>
+            {srcPreview2 && <img src={`${srcPreview2}`} alt="Gambar Content" className='gambar-content'/>}
+            <input 
+              type="file" 
+              name='gambar2' 
+              onChange={e => {
+                const newData = [...file];
+                if(newData.length > 0){
+                  newData[1] = e.target.files[0];
+                }else if(newData.length === 0){
+                  newData.push(e.target.files[0]);
+                }
+                setFile(newData);
+                const objectUrl = URL.createObjectURL(e.target.files[0])
+                setSrcPreview2(objectUrl);
+              }}
+            />
+          </section>
+        </div>
+        <label htmlFor="main-content">Isi Content</label>
+        <ReactQuill 
+          theme="snow"  
+          value={desc} 
+          onChange={setDesc} 
+          modules={modules} 
+          formats={formats} 
+          name='main-content'
+        />
+        <div className="form-button">
+          <Button>Submit</Button>
+        </div>
+      </form>
+    </>
   )
 }
 
