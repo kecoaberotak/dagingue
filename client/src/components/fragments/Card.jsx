60318cb3ab01f6e124dd17940ea965c8401fb0ec
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-08-09 19:31:59 +0700

    tampilan jenis potong udah kelar

--- client/src/components/fragments/Card.jsx
+++ client/src/components/fragments/Card.jsx
@@ -1,13 +1,13 @@
 const Card = (props) => {
-  const {tipe, berat, image} = props
+  const {tipe, berat, image, key} = props
   return(
-    <div className="card">
+    <div className="card" key={key}>
       <section className="card-product">
           <img className="potong-1" src={image} alt="404" />
       </section>
       <section className="card-product-info">
         <p>{tipe}</p>
-        <p>{berat} gram</p>
+        <p>{berat}</p>
       </section>
     </div>
   );
--- client/src/components/layouts/ClientProductPotong.jsx
+++ client/src/components/layouts/ClientProductPotong.jsx
@@ -1,18 +1,39 @@
 import Title from "../elements/Title";
 import Card from "../fragments/Card";
-
-import '../../assets/landing-page-css/productPotong.css'
+import { getPotong } from "../../services/potong-service";
+import '../../assets/landing-page-css/productPotong.css';
+import { useEffect, useState } from "react";
 
 const ProductPotong = () => {
-  return(
-    <div className="product-potong">
-      <Title classname='title-section potong' title='Produk' subTitle='Jenis Potongan' />
-      <section className="cards-product">
-        <Card tipe={'Slice Dadu'} berat={500} image={"./images/product-3.jpg"} />
-        <Card tipe={'Slice Tipis'} berat={500} image={"./images/product-1.jpg"} />
-      </section>
-    </div>
-  );
+  const [data, setData] = useState();
+  
+  useEffect(() => {
+    getPotong(res => setData(res.data));
+  }, []);
+
+
+  if(data){
+    let classname = '';
+    if(data.length > 2){
+      classname = "cards-product-3"
+    }else if(data.length < 2){
+      classname = "cards-product-1"
+    }else classname = "cards-product-2"
+
+    return(
+      <div className="product-potong">
+        <Title classname='title-section potong' title='Produk' subTitle='Jenis Potongan' />
+        <section className={classname}>
+        {data.map(potong => {
+          return (
+              <Card key={potong._id} tipe={potong.title} berat={potong.desc} image={`http://localhost:4000/${potong.file}`} />
+          )
+        })}
+            </section>
+      </div>
+    );
+  }
+
 };
 
 export default ProductPotong;
