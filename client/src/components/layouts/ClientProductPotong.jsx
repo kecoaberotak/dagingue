356892572e80e11900b6562681b3b89e6b7a2aa9
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-08-10 14:48:23 +0700

    edit load buat ui potong

--- client/src/components/layouts/ClientProductPotong.jsx
+++ client/src/components/layouts/ClientProductPotong.jsx
@@ -36,29 +36,6 @@ const ProductPotong = () => {
       }
     </div>
   );
-
-  // if(data){
-  //   let classname = '';
-  //   if(data.length > 2){
-  //     classname = "cards-product-3"
-  //   }else if(data.length < 2){
-  //     classname = "cards-product-1"
-  //   }else classname = "cards-product-2"
-
-  //   return(
-  //     <div className="product-potong">
-  //       <Title classname='title-section potong' title='Produk' subTitle='Jenis Potongan' />
-  //       <section className={classname}>
-  //       {data.map(potong => {
-  //         return (
-  //           <Card key={potong._id} tipe={potong.title} berat={potong.desc} image={`http://localhost:4000/${potong.file}`} />
-  //         )
-  //       })}
-  //       </section>
-  //     </div>
-  //   );
-  // }
-
 };
 
 export default ProductPotong;
