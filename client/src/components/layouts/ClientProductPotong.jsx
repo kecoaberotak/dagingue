ed5e6c873119786c15b31ba4d2809e9fac060a0a
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-08-10 14:47:00 +0700

    bikin load buat ui potong

--- client/src/components/layouts/ClientProductPotong.jsx
+++ client/src/components/layouts/ClientProductPotong.jsx
@@ -6,33 +6,58 @@ import { useEffect, useState } from "react";
 
 const ProductPotong = () => {
   const [data, setData] = useState();
+  let classname = '';
   
   useEffect(() => {
     getPotong(res => setData(res.data));
   }, []);
 
-
-  if(data){
-    let classname = '';
+  if(data) {
     if(data.length > 2){
       classname = "cards-product-3"
     }else if(data.length < 2){
       classname = "cards-product-1"
     }else classname = "cards-product-2"
+  }
 
-    return(
-      <div className="product-potong">
-        <Title classname='title-section potong' title='Produk' subTitle='Jenis Potongan' />
+  return(
+    <div className="product-potong">
+      <Title classname='title-section potong' title='Produk' subTitle='Jenis Potongan' />
+      {data ? 
         <section className={classname}>
         {data.map(potong => {
           return (
-              <Card key={potong._id} tipe={potong.title} berat={potong.desc} image={`http://localhost:4000/${potong.file}`} />
+            <Card key={potong._id} tipe={potong.title} berat={potong.desc} image={`http://localhost:4000/${potong.file}`} />
           )
         })}
-            </section>
-      </div>
-    );
-  }
+        </section>
+      :
+        <div className="load-card"></div>
+      }
+    </div>
+  );
+
+  // if(data){
+  //   let classname = '';
+  //   if(data.length > 2){
+  //     classname = "cards-product-3"
+  //   }else if(data.length < 2){
+  //     classname = "cards-product-1"
+  //   }else classname = "cards-product-2"
+
+  //   return(
+  //     <div className="product-potong">
+  //       <Title classname='title-section potong' title='Produk' subTitle='Jenis Potongan' />
+  //       <section className={classname}>
+  //       {data.map(potong => {
+  //         return (
+  //           <Card key={potong._id} tipe={potong.title} berat={potong.desc} image={`http://localhost:4000/${potong.file}`} />
+  //         )
+  //       })}
+  //       </section>
+  //     </div>
+  //   );
+  // }
 
 };
 
