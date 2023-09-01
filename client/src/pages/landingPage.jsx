db136afa41b91b21fafefccc865b95ded3014e21
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-09-01 22:36:00 +0700

    bagian header pake tailwind, tapi belum kelar

--- client/src/pages/landingPage.jsx
+++ client/src/pages/landingPage.jsx
@@ -1,23 +1,19 @@
 import Header from "../components/layouts/Header";
-import About from "../components/layouts/About";
-import Products from "../components/layouts/Products";
-import Footer from "../components/layouts/Footer";
+// import About from "../components/layouts/About";
+// import Products from "../components/layouts/Products";
+// import Footer from "../components/layouts/Footer";
 
 // CSS
-// import '../assets/landing-page-css/landingPage.css'
 import '../index.css'
 
 const LandingPage = () => {
   return (
-    <div className="container">
-    <h1>Header 1</h1>
-    <h2>Header 2</h2>
-    <h3>Header 3</h3>
+    <>
       <Header></Header>
       {/* <About></About> */}
       {/* <Products></Products> */}
       {/* <Footer></Footer> */}
-    </div>
+    </>
   );
 };
 
