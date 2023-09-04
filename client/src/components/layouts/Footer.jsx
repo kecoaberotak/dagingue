00d436df8fdda6ed6e83a39ccc0a00675801367b
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-09-04 12:24:19 +0700

    rapihin code

--- client/src/components/layouts/Footer.jsx
+++ client/src/components/layouts/Footer.jsx
@@ -1,8 +1,5 @@
 import FooterBanner from "../fragments/FooterBanner";
 import FooterContact from "../fragments/FooterContact";
-
-// import '../../assets/landing-page-css/footer.css'
-
 const Footer = () => {
   return(
     <footer className="footer" id="contact">
