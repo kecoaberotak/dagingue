2387cd06a55b60b2d2f040bff0aa5bfac36452fc
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-09-12 19:26:55 +0700

    add load skeleton gambar potong

--- client/src/components/fragments/Card.jsx
+++ client/src/components/fragments/Card.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+
 const Card = (props) => {
   const {tipe, berat, image} = props
+  const [load, setLoad] = useState(false);
+
+  const onLoad = () => {
+    setLoad(true);
+  }
+
   return(
     <div className="card">
       <section className="card-product">
-          <img className="potong-img" src={image} alt="404" />
+          <img className={`potong-img ${load ? '' : 'hidden'}`} src={image} alt="404" onLoad={onLoad}/>
+          {!load && <Skeleton height={150} />}
       </section>
       <section className="card-product-info">
         <p>{tipe}</p>
