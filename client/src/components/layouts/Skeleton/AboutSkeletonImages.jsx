d5ed2b9cb3114288099b7e056ad8b690f7fbf8d4
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-09-13 19:21:04 +0700

    benerin ui AboutSkeletonImages

--- client/src/components/layouts/Skeleton/AboutSkeletonImages.jsx
+++ client/src/components/layouts/Skeleton/AboutSkeletonImages.jsx
@@ -4,10 +4,10 @@ import 'react-loading-skeleton/dist/skeleton.css';
 const AboutSkeletonImages = () => {
   return (
     <>
-    <div className='foto-1'>
+    <div className='foto-1 border-none'>
       <Skeleton height={250}></Skeleton>
     </div>
-    <div className='foto-2'>
+    <div className='foto-2 border-none'>
       <Skeleton height={200}></Skeleton>
     </div>
     </>
