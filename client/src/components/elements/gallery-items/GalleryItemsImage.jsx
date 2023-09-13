64006f9d98b33d104ab05242235cb5ced3efd1af
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-09-13 19:41:33 +0700

    add skeleton di GalleryItemsImage

--- client/src/components/elements/gallery-items/GalleryItemsImage.jsx
+++ client/src/components/elements/gallery-items/GalleryItemsImage.jsx
@@ -1,12 +1,15 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ImageSelected } from "../../../contexts/ImageSelected";
 import { DescSelected } from "../../../contexts/DescSelected";
 import { TitleSelected } from "../../../contexts/TitleSelected";
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 const GalleryItemsImage = (props) => {
   const {setSelectedImage} = useContext(ImageSelected);
   const {setDescSelected} = useContext(DescSelected);
   const {setTitleSelected} = useContext(TitleSelected);
+  const [load, setLoad] = useState(false);
 
   const selectImage = (e) => {
     setSelectedImage(e.target.getAttribute('data-image'))
@@ -22,11 +25,20 @@ const GalleryItemsImage = (props) => {
     e.target.classList.add('selected');
   };
 
+  const onLoad = () => {
+    setLoad(true);
+  }
+
   const {image, title, desc} = props;
   return (
     <div className="gallery-item-image" onClick={selectImage}>
-      <img src={image} alt="404" />
-      <div className='cover' data-title={title} data-desc={desc} data-image={image}></div>
+      <img src={image} alt="404" onLoad={onLoad} className={`${load ? '' : 'hidden'}`} />
+      {!load && 
+        <div className="w-[66px] h-[66px] sm:w-[132px] sm:h-[132px]">
+          <Skeleton height={`100%`} />
+        </div>
+      }
+      <div className={`cover ${load ? '' : 'hidden'}`} data-title={title} data-desc={desc} data-image={image}></div>
     </div>
   );
 };
