bc8f6d14c64170c0c8909467e507c371dec42d41
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-09-13 18:58:56 +0700

    benerin css dan tambah skeleton GalleryInfoImage

--- client/src/components/elements/gallery-info/GalleryInfoImage.jsx
+++ client/src/components/elements/gallery-info/GalleryInfoImage.jsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+
 const GalleryInfoImage = (props) => {
+  const [load, setLoad] = useState(false);
+
+  const onLoad = () => {
+    setLoad(true);
+  }
+
+  useEffect(()=>{
+    console.log(load);
+  }, [load])
+
   const {image} = props;
   return(
     <div className="gallery-info-image">
-      <img src={image} alt="404" />
+      <img src={image} alt="404" onLoad={onLoad} className={`${load ? '' : 'hidden'}`}/>
+      {!load && <Skeleton height={`98%`} />}
     </div>
   );
 };
