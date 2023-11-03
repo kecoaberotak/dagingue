109e26f535afa39aedada9c31c0ea55183dd0f38
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-11-03 19:46:12 +0700

    add title untuk component logo

--- client/src/components/elements/Logo.jsx
+++ client/src/components/elements/Logo.jsx
@@ -2,7 +2,7 @@ const Logo = (props) => {
   const {classname, image} = props;
   return(
     <div className={classname}>
-      <img src={image} alt="Logo Dagingue" />
+      <img src={image} alt="Logo Dagingue" title="Logo Dagingue" />
     </div>
   );
 };
