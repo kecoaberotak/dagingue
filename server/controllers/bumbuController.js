9bb8c248517e6c1a5c5b150149c57d3c5a48f66c
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-01-20 18:32:20 +0700

    testing input body file dan link

--- server/controllers/bumbuController.js
+++ server/controllers/bumbuController.js
@@ -85,10 +85,10 @@ const updateBumbu = asyncHandler(async (req, res) => {
     throw new Error('Please add description')
   }
 
-  console.log(req.body.link, 'link');
+  // console.log(req.body.link, 'link');
   console.log(req.body.file, 'body file');
-  console.log(req.files, 'files');
-  console.log(req.body.link === req.body.file, 'pengecekan');
+  // console.log(req.files, 'files');
+  console.log(req.body.link == req.body.file, 'pengecekan');
 
   // if(!req.file) {
   //   res.status(400);
