6e08863b79156587b1f5a5525f2d9e035d9204e5
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-01-21 14:13:51 +0700

    edit navbar menu

--- client/src/components/layouts/Header.jsx
+++ client/src/components/layouts/Header.jsx
@@ -6,7 +6,6 @@ const Header = () => {
     <header className="header" id="home">
       <Logo classname='logo' image='/Logo.png' />
       <ul className="navbar-header">
-        <li><a href="#home">Beranda</a></li>
         <li><a href="#about">Tentang Kami</a></li>
         <li><a href="#product">Produk</a></li>
         <li><a href="#contact">Kontak</a></li>
