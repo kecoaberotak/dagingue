82e15edd8225c541e059a5a0e7568699d59325d2
kecoaberotak <dennisfernandes0212@gmail.com>
2024-03-10 12:22:09 +0700

    testing udah jalan dan bikin testid buat content di article about

--- client/src/__tests__/components/About.test.jsx
+++ client/src/__tests__/components/About.test.jsx
@@ -3,9 +3,25 @@ import { render, screen } from "@testing-library/react";
 import About from "../../components/layouts/About";
 
 describe("About Component", () => {
-  render(<About />);
+  const renderComponents = () => {
+    render(<About />);
+
+    return {
+      article: screen.getByRole("article"),
+      images: screen.getAllByRole("img"),
+    };
+  };
+
   it("should render article", () => {
-    const article = screen.getByRole("article");
+    const { article } = renderComponents();
+
     expect(article).toBeInTheDocument();
+  });
+
+  it("should render image", () => {
+    const { images } = renderComponents();
+
+    expect(images[0]).toBeInTheDocument();
+    expect(images[1]).toBeInTheDocument();
   });
 });
--- client/src/components/layouts/About.jsx
+++ client/src/components/layouts/About.jsx
@@ -11,12 +11,12 @@ const About = () => {
   const [load, setLoad] = useState(false);
 
   useEffect(() => {
-    getContent(res => setData(res.data[0]));
+    getContent((res) => setData(res.data[0]));
   }, []);
 
   useEffect(() => {
-    if(data){
-      setContent(data.content)
+    if (data) {
+      setContent(data.content);
       setSrcPreview(data.file1);
       setSrcPreview2(data.file2);
     }
@@ -24,25 +24,39 @@ const About = () => {
 
   const onLoad = () => {
     setLoad(true);
-  }
+  };
 
   return (
     <Fragment>
       <section className="about" id="about">
         <div className="image-about">
-          <img src={srcPreview} alt="Tentang Dagingue" title="Tentang Dagingue" className={`foto-1 ${load ? '' : 'hidden'}`} onLoad={onLoad}/>
-          <img src={srcPreview2} alt="Produk Dagingue" title="Produk Dagingue" className={`foto-2 ${load ? '' : 'hidden'}`} />
+          <img
+            src={srcPreview}
+            alt="Tentang Dagingue"
+            title="Tentang Dagingue"
+            className={`foto-1 ${load ? "" : "hidden"}`}
+            onLoad={onLoad}
+          />
+          <img
+            src={srcPreview2}
+            alt="Produk Dagingue"
+            title="Produk Dagingue"
+            className={`foto-2 ${load ? "" : "hidden"}`}
+          />
           {!load && <AboutSkeletonImages />}
         </div>
         <article className="article-about">
-          {content ?
-          <>
-            <h1>Tentang Dagingue Bogor</h1>
-            <div dangerouslySetInnerHTML={{__html:content}}></div>
-          </>
-          :
-          <AboutArticleSkeleton />
-          }
+          {content ? (
+            <>
+              <h1>Tentang Dagingue Bogor</h1>
+              <div
+                dangerouslySetInnerHTML={{ __html: content }}
+                data-testid="article-content"
+              ></div>
+            </>
+          ) : (
+            <AboutArticleSkeleton />
+          )}
         </article>
       </section>
     </Fragment>
