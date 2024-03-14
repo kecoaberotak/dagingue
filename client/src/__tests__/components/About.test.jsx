1014dd6c9033f33b545a6c1a549baa2154db3204
kecoaberotak <dennisfernandes0212@gmail.com>
2024-03-14 21:52:16 +0700

    rapihin code

--- client/src/__tests__/components/About.test.jsx
+++ client/src/__tests__/components/About.test.jsx
@@ -4,36 +4,55 @@ import { describe, it, expect } from "vitest";
 import About from "../../components/layouts/About";
 
 describe("About Component", () => {
-  it("should render heading when fetching data success", async () => {
+  const renderElements = async () => {
     render(<About />);
 
-    const heading = await screen.findByRole("heading", {
-      name: /tentang dagingue bogor/i,
-    });
+    return {
+      heading: await screen.findByRole("heading", {
+        name: /tentang dagingue bogor/i,
+      }),
+      img1: screen.getByRole("img", { name: /tentang dagingue/i }),
+      img2: screen.getByRole("img", { name: /produk dagingue/i }),
+      skeletonImg1: screen.getByTestId("skeleton-image-1"),
+      skeletonImg2: screen.getByTestId("skeleton-image-2"),
+    };
+  };
+
+  it("should render heading when fetching data success", async () => {
+    const { heading } = await renderElements();
+
     expect(heading).toBeInTheDocument();
   });
 
   it("should call onload event image when data fetching success", async () => {
-    render(<About />);
+    const { img1, img2 } = await renderElements();
 
-    const img1 = screen.getByRole("img", { name: /tentang dagingue/i });
     fireEvent.load(img1);
     await waitFor(() => expect(img1).toHaveClass("foto-1"));
 
-    const img2 = screen.getByRole("img", { name: /produk dagingue/i });
     fireEvent.load(img2);
     await waitFor(() => expect(img2).toHaveClass("foto-2"));
   });
 
-  it("should render skeleton for article when data not loaded", () => {
-    render(<About />);
+  it("should render skeleton for image when data not loaded", async () => {
+    const { skeletonImg1, skeletonImg2 } = await renderElements();
 
-    const skeletonImg1 = screen.getByTestId("skeleton-image-1");
     expect(skeletonImg1).toBeInTheDocument();
-
-    const skeletonImg2 = screen.getByTestId("skeleton-image-2");
     expect(skeletonImg2).toBeInTheDocument();
-
-    screen.debug();
   });
+
+  it("should not render skeleton for image when data is loaded", async () => {
+    const { skeletonImg1, skeletonImg2, img1, img2 } = await renderElements();
+
+    fireEvent.load(img1);
+    await waitFor(() => expect(img1).toHaveClass("foto-1"));
+
+    fireEvent.load(img2);
+    await waitFor(() => expect(img2).toHaveClass("foto-2"));
+
+    expect(skeletonImg1).not.toBeInTheDocument();
+    expect(skeletonImg2).not.toBeInTheDocument();
+  });
+
+  // tinggal skeleton buat article
 });
