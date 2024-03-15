999162f8eeb4c1f89eb53913e535355ee6faf54d
kecoaberotak <dennisfernandes0212@gmail.com>
2024-03-15 14:17:11 +0700

    test header kelar

--- client/src/__tests__/layouts/Header.test.jsx
+++ client/src/__tests__/layouts/Header.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 
 import Header from "../../components/layouts/Header";
@@ -7,9 +7,20 @@ describe("Header Component", () => {
   it("should render header", () => {
     render(<Header />);
 
-    const element = screen.getByText(/produk/i);
-    expect(element).toBeInTheDocument();
+    const banner = screen.getByRole("banner");
+    expect(banner).toBeInTheDocument();
 
-    // check element lain
+    const logo = within(banner).getByRole("img", { name: /logo dagingue/i });
+    expect(logo).toBeInTheDocument();
+
+    const link1 = screen.getByRole("link", { name: /tentang kami/i });
+    const link2 = screen.getByRole("link", { name: /produk/i });
+    const link3 = screen.getByRole("link", { name: /kontak/i });
+    expect(link1, link2, link3).toBeInTheDocument();
+
+    const instagram = within(banner).getByRole("img", { name: /instagram/i });
+    const whatsapp = within(banner).getByRole("img", { name: /whatsapp/i });
+    const shopee = within(banner).getByRole("img", { name: /shopee/i });
+    expect(instagram, whatsapp, shopee).toBeInTheDocument();
   });
 });
