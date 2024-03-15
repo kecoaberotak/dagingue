7b2f311a8aa9ac704bfc29fa7b1d877d00440e7d
kecoaberotak <dennisfernandes0212@gmail.com>
2024-03-15 13:12:08 +0700

    testing header kelar

---
+++ client/src/__tests__/layouts/Header.test.jsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+
+import Header from "../../components/layouts/Header";
+
+describe("Header Component", () => {
+  it("should render header", () => {
+    render(<Header />);
+
+    const element = screen.getByText(/produk/i);
+    expect(element).toBeInTheDocument();
+  });
+});
