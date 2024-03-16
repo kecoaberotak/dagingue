e174d9614376ae2e46e73be9b147a857cb62e7b4
kecoaberotak <dennisfernandes0212@gmail.com>
2024-03-16 20:46:28 +0700

    testing image buat GalleryInfoImage

--- client/src/__tests__/layouts/ClientProductBumbu.test.jsx
+++ client/src/__tests__/layouts/ClientProductBumbu.test.jsx
@@ -1,4 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import {
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+  within,
+} from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 
 import ProductBumbu from "../../components/layouts/ClientProductBumbu";
@@ -27,16 +33,32 @@ describe("ProductBumbu Component", () => {
     };
   };
 
+  //   GalleryInfo
   it("should render title and subtitle", () => {
     const { title, subTitle } = renderComponent();
 
     expect(title, subTitle).toBeInTheDocument();
   });
 
-  it("should skeleton when dataBumbu is not provided", () => {
+  it("should render skeleton for gallery info when dataBumbu is not provided", () => {
     renderComponent();
     const skeleton = screen.getByTestId("skeleton-gallery-info");
 
     expect(skeleton).toBeInTheDocument();
   });
+
+  //   GaleryInfoImage
+  it("should render image for GalleryInfoImage if data provided and image has loaded", async () => {
+    renderComponent();
+
+    const container = await screen.findByTestId("gallery-info-image");
+
+    const img = within(container).getByRole("img");
+    expect(img).toBeInTheDocument();
+
+    fireEvent.load(img);
+    await waitFor(() => expect(img).toHaveClass(/default/i));
+  });
+
+  // kalo gambar belom load, render skeleton
 });
--- client/src/components/elements/gallery-info/GalleryInfoImage.jsx
+++ client/src/components/elements/gallery-info/GalleryInfoImage.jsx
@@ -1,18 +1,24 @@
 import { useState } from "react";
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const GalleryInfoImage = (props) => {
   const [load, setLoad] = useState(false);
 
   const onLoad = () => {
     setLoad(true);
-  }
+  };
 
-  const {image, title} = props;
-  return(
-    <div className="gallery-info-image">
-      <img src={image} alt={title} title={title} onLoad={onLoad} className={`${load ? '' : 'hidden'}`}/>
+  const { image, title } = props;
+  return (
+    <div className="gallery-info-image" data-testid="gallery-info-image">
+      <img
+        src={image}
+        alt={title}
+        title={title}
+        onLoad={onLoad}
+        className={`${load ? "default" : "hidden"}`}
+      />
       {!load && <Skeleton height={`98%`} />}
     </div>
   );
