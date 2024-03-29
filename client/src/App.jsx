89af77555e73045db346366a18fd14b0f2265ddc
kecoaberotak <dennisfernandes0212@gmail.com>
2024-03-29 15:40:14 +0700

    benerin struktur file, pindahin entry point ke App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,47 @@
+// contexts
+import AdminInfoProvider from "./contexts/AdminInfo";
+import LoginStatusProvider from "./contexts/LoginStatus";
+
+// css
+import "./index.css";
+
+// Pages
+import LandingPage from "./pages/landingPage";
+import ErrorPage from "./pages/errorPage";
+import AdminPage from "./pages/adminPage";
+import LoginPage from "./pages/loginPage";
+
+// Middleware
+import { HelmetProvider } from "react-helmet-async";
+
+// Router
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/admin",
+    element: <AdminPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+]);
+
+const App = () => {
+  return (
+    <LoginStatusProvider>
+      <AdminInfoProvider>
+        <HelmetProvider>
+          <RouterProvider router={router} />
+        </HelmetProvider>
+      </AdminInfoProvider>
+    </LoginStatusProvider>
+  );
+};
+
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,46 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AdminInfoProvider from './contexts/AdminInfo'
-import LoginStatusProvider from './contexts/LoginStatus'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-// css
-import './index.css';
-
-// Pages
-import LandingPage from './pages/landingPage'
-import ErrorPage from './pages/errorPage'
-import AdminPage from './pages/adminPage'
-import LoginPage from './pages/loginPage'
-
-// Middleware
-import {HelmetProvider} from "react-helmet-async"
-
-// Router
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <LandingPage />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: '/admin',
-    element: <AdminPage />,
-  },
-  {
-    path: '/login',
-    element: <LoginPage />,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <LoginStatusProvider>
-      <AdminInfoProvider>
-        <HelmetProvider>
-          <RouterProvider router={router} />
-        </HelmetProvider>
-      </AdminInfoProvider>
-    </LoginStatusProvider>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
