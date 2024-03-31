64f472257c9bdd789bee950e8ee6c92f32d20f3c
kecoaberotak <dennisfernandes0212@gmail.com>
2024-03-31 13:45:42 +0700

    testing route buat landing page dan error page, hapus test landingPage, dan benerin code di errorPage

--- client/src/__tests__/pages/App.test.jsx
+++ client/src/__tests__/pages/App.test.jsx
@@ -1,12 +1,39 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
-import App from "../../App";
-import { BrowserRouter, MemoryRouter } from "react-router-dom";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import routesConfig from "../../routes/routesConfig";
+
+import LoginStatusProvider from "../../contexts/LoginStatus";
+import AdminInfoProvider from "../../contexts/AdminInfo";
+import { HelmetProvider } from "react-helmet-async";
 
 describe("App entry point", () => {
-  it("should landing on landing page", () => {
-    render(<App />);
+  const renderRoutes = (route) => {
+    const router = createMemoryRouter(routesConfig, {
+      initialEntries: [route],
+    });
 
-    screen.debug();
+    render(
+      <LoginStatusProvider>
+        <AdminInfoProvider>
+          <HelmetProvider>
+            <RouterProvider router={router} />
+          </HelmetProvider>
+        </AdminInfoProvider>
+      </LoginStatusProvider>
+    );
+  };
+
+  it("should open landing page", () => {
+    renderRoutes("/");
+
+    expect(screen.getByRole("link", { name: /produk/i })).toBeInTheDocument();
+    expect(screen.getByRole("img", { name: /tentang/i })).toBeInTheDocument();
+  });
+
+  it("should open errorPage when route not provided ", () => {
+    renderRoutes("/aa");
+
+    expect(screen.getByTestId("errorpage")).toBeInTheDocument();
   });
 });
