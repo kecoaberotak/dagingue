193000256e97bd0a05b2f9b58d13345ecdbdac67
kecoaberotak <dennisfernandes0212@gmail.com>
2024-04-03 14:24:05 +0700

    bikin test buat EditAbout

---
+++ client/src/__tests__/layouts/Admin/EditAbout.test.jsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+
+import AboutContent from "../../../components/layouts/Admin/AboutContent";
+import DisplayStatusProvider from "../../../contexts/DisplayStatus";
+
+describe("AboutContent", () => {
+  const renderComponent = () =>
+    render(
+      <DisplayStatusProvider>
+        <AboutContent />
+      </DisplayStatusProvider>
+    );
+
+  it("should render EditAbout", () => {
+    renderComponent();
+
+    const headingAbout = screen.getByRole("heading", {
+      name: /about/i,
+    });
+
+    expect(headingAbout).toBeInTheDocument();
+
+    screen.debug();
+  });
+  //   test lagi lebih banyak
+});
