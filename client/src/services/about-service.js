60f20879e80a86a12f262b091e6c010e5e26b5b1
kecoaberotak <dennisfernandes0212@gmail.com>
2024-05-26 15:43:00 +0700

    ganti url about-service.js pake url local

--- client/src/services/about-service.js
+++ client/src/services/about-service.js
@@ -1,34 +1,47 @@
 import axios from "axios";
-const apiUrl = import.meta.env.VITE_DAGINGUE_API_URL;
+// const apiUrl = import.meta.env.VITE_DAGINGUE_API_URL;
+const apiUrl = "http://localhost:4000";
 
 export const getContent = (callback) => {
-  axios.get(apiUrl + '/api/about').then(res => {
-    callback(res.data);
-  }).catch(err => {
-    callback(err.response);
-  })
+  axios
+    .get(apiUrl + "/api/about")
+    .then((res) => {
+      callback(res.data);
+    })
+    .catch((err) => {
+      callback(err.response);
+    });
 };
 
 export const addContent = (data, callback) => {
-  axios.post(apiUrl + '/api/about', data).then(res => {
-    callback(res);
-  }).catch(err => {
-    callback(err.response);
-  })
+  axios
+    .post(apiUrl + "/api/about", data)
+    .then((res) => {
+      callback(res);
+    })
+    .catch((err) => {
+      callback(err.response);
+    });
 };
 
 export const getDetailContent = (id, callback) => {
-  axios.get(apiUrl + '/api/about/' + id).then(res => {
-    callback(res);
-  }).catch(err => {
-    callback(err.response);
-  });
+  axios
+    .get(apiUrl + "/api/about/" + id)
+    .then((res) => {
+      callback(res);
+    })
+    .catch((err) => {
+      callback(err.response);
+    });
 };
 
 export const putContent = (id, data, callback) => {
-  axios.put(apiUrl + '/api/about/' + id, data).then(res => {
-    callback(res);
-  }).catch(err => {
-    callback(err.response);
-  });
+  axios
+    .put(apiUrl + "/api/about/" + id, data)
+    .then((res) => {
+      callback(res);
+    })
+    .catch((err) => {
+      callback(err.response);
+    });
 };
