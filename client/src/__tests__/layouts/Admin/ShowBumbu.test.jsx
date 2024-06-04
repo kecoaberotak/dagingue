46047d1f2e28b63ecb02b58d57f6d6af1b47f321
kecoaberotak <dennisfernandes0212@gmail.com>
2024-06-04 11:00:32 +0700

    ShowBumbu.test.jsx - berhasil test tombol edit

--- client/src/__tests__/layouts/Admin/ShowBumbu.test.jsx
+++ client/src/__tests__/layouts/Admin/ShowBumbu.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
+import userEvent from "@testing-library/user-event";
 
 import ShowBumbu from "../../../components/layouts/Admin/ShowBumbu";
 import IdBumbuProvider from "../../../contexts/IdBumbu";
@@ -21,9 +22,12 @@ describe("ShowBumbu", () => {
   };
 
   it("should render ShowBumbu Page", async () => {
+    const user = userEvent.setup();
     const { buttonsEdit } = await renderElements();
 
     expect(buttonsEdit[0]).toBeInTheDocument();
+
+    await user.click(buttonsEdit[0]);
     screen.debug();
   });
 });
