98737fd49b93b5ac9136c052e20631abd2a77a78
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-06-24 16:41:19 +0700

    benerin code getPotong

--- client/src/components/layouts/ClientProductPotong.jsx
+++ client/src/components/layouts/ClientProductPotong.jsx
@@ -6,37 +6,41 @@ import PotongSkeleton from "./Skeleton/PotongSkeleton";
 
 const ProductPotong = () => {
   const [data, setData] = useState();
-  let classname = '';
-  
+  let classname = "";
+
   useEffect(() => {
-    getPotong(res => setData(res.data));
+    getPotong((res) => {
+      if (res.status === 200) {
+        setData(res.data.data);
+      } else if (res.status === 400) {
+        alert(res.data.message);
+      }
+    });
   }, []);
 
-  if(data) {
-    if(data.length > 2){
-      classname = "cards-product-3"
-    }else if(data.length < 2){
-      classname = "cards-product-1"
-    }else classname = "cards-product-2"
+  if (data) {
+    if (data.length > 2) {
+      classname = "cards-product-3";
+    } else if (data.length < 2) {
+      classname = "cards-product-1";
+    } else classname = "cards-product-2";
   }
 
-  return(
+  return (
     <div className="product-potong">
-      <Title classname='title-section potong' title='Produk' subTitle='Jenis Potongan' />
-      {data ? 
+      <Title classname="title-section potong" title="Produk" subTitle="Jenis Potongan" />
+      {data ? (
         <section className={classname}>
-        {data.map(potong => {
-          return (
-            <Card key={potong._id} tipe={potong.title} berat={potong.desc} image={potong.file} />
-          )
-        })}
+          {data.map((potong) => {
+            return <Card key={potong._id} tipe={potong.title} berat={potong.desc} image={potong.file} />;
+          })}
         </section>
-      :
+      ) : (
         <section className="cards-product-2">
           <PotongSkeleton />
           <PotongSkeleton />
         </section>
-      }
+      )}
     </div>
   );
 };
--- client/src/services/potong-service.js
+++ client/src/services/potong-service.js
@@ -2,41 +2,56 @@ import axios from "axios";
 const apiUrl = import.meta.env.VITE_DAGINGUE_API_URL;
 
 export const addPotong = (data, callback) => {
-  axios.post(apiUrl + '/api/potong', data).then(res => {
-    callback(res);
-  }).catch(err => {
-    callback(err.response);
-  })
+  axios
+    .post(apiUrl + "/api/potong", data)
+    .then((res) => {
+      callback(res);
+    })
+    .catch((err) => {
+      callback(err.response);
+    });
 };
 
 export const getPotong = (callback) => {
-  axios.get(apiUrl + '/api/potong').then(res => {
-    callback(res.data);
-  }).catch(err => {
-    callback(err.response);
-  })
+  axios
+    .get(apiUrl + "/api/potong")
+    .then((res) => {
+      callback(res);
+    })
+    .catch((err) => {
+      callback(err.response);
+    });
 };
 
 export const deletePotong = (id, callback) => {
-  axios.delete(apiUrl + '/api/potong/' + id).then(res => {
-    callback(res);
-  }).catch(err => {
-    callback(err.response);
-  });
+  axios
+    .delete(apiUrl + "/api/potong/" + id)
+    .then((res) => {
+      callback(res);
+    })
+    .catch((err) => {
+      callback(err.response);
+    });
 };
 
 export const getDetailPotong = (id, callback) => {
-  axios.get(apiUrl + '/api/potong/' + id).then(res => {
-    callback(res);
-  }).catch(err => {
-    callback(err.response);
-  });
+  axios
+    .get(apiUrl + "/api/potong/" + id)
+    .then((res) => {
+      callback(res);
+    })
+    .catch((err) => {
+      callback(err.response);
+    });
 };
 
 export const putPotong = (id, data, callback) => {
-  axios.put(apiUrl + '/api/potong/' + id, data).then(res => {
-    callback(res);
-  }).catch(err => {
-    callback(err.response);
-  });
-}
+  axios
+    .put(apiUrl + "/api/potong/" + id, data)
+    .then((res) => {
+      callback(res);
+    })
+    .catch((err) => {
+      callback(err.response);
+    });
+};
