a9da17526983a3a9161dfd1f1512ec5b33eb9e0f
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-08-07 12:56:37 +0700

    edit UI Footer Landing Page

--- client/src/components/fragments/FooterContact.jsx
+++ client/src/components/fragments/FooterContact.jsx
@@ -2,20 +2,30 @@ import Socmed from "./Socmed";
 import Logo from "../elements/Logo";
 
 const FooterContact = () => {
-  return(
+  return (
     <section className="footer-contact">
       <div className="contact-info">
         <section className="contact-info-main">
-          <p><a href="mailto:dennisfernandes0212@gmail.com">dagingue@gmail.com</a></p>
+          <p>
+            <a href="mailto:dennisfernandes0212@gmail.com">dagingue@gmail.com</a>
+          </p>
           <p>085716059444</p>
-          <p>Perumahan Bumi Menteng Asri<br />
-            Blok BN 20. RT 02  RW 03<br />
-            Bogor, Jawa Barat</p>
-            <p><a href="https://goo.gl/maps/ypBTX8cgGmkmHZGUA" target="_blank" rel="noreferrer">Lihat pada peta</a></p>
+          <p>
+            Perumahan Bumi Menteng Asri
+            <br />
+            Blok BN 20. RT 02 RW 03
+            <br />
+            Bogor, Jawa Barat
+          </p>
+          <p>
+            <a href="https://goo.gl/maps/ypBTX8cgGmkmHZGUA" target="_blank" rel="noreferrer">
+              <u>Lihat pada peta</u>
+            </a>
+          </p>
         </section>
         <Socmed classname="contact-info-socmed" />
       </div>
-      <Logo classname='contact-logo' />
+      <Logo classname="contact-logo" />
     </section>
   );
 };
